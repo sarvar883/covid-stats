f5acd9e521b971f197778d3ce999728df123b03c
Sarvar <sarvarik883@gmail.com>
2021-01-27 22:32:06 +0500

    added overflow on small screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  // HashRouter as Router,
   BrowserRouter as Router,
   Route,
   Switch
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary pt-0 pb-0" id="navbar">
       <div className="container pt-2 pb-2 pl-0 pr-0">
-        <Link to='/' className="navbar-brand p-2">COVID Stat App</Link>
+        <Link to='/' className="navbar-brand p-2">COVID Stats App</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,8 +21,8 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav mr-auto list-inline">
             <li className="nav-item list-inline-item">
-              <Link className="nav-link" to="/map">
-                Map
+              <Link className="nav-link" to="/404">
+                Not found page
               </Link>
             </li>
           </ul>
